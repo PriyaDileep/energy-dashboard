620cd4a77ee76fb4053d1625fa160bba24485646
Priyanka <ps.kolli@gmail.com>
2026-04-18 12:52:52 +1000

    chore: initial Xcode project setup with Swift gitignore

---
+++ EnergyDashboard/EnergyDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnergyDashboardApp.swift
+//  EnergyDashboard
+//
+//  Created by Priyanka on 18/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct EnergyDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
